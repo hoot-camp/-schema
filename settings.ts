457e2af9b532b1a718cf252768869eb297f599f5
isaac-110 <isaac-110@hi16.ca>
2023-04-11 01:29:05 +0000

    update: name to datumJoinDot

--- settings.ts
+++ settings.ts
@@ -1,8 +1,8 @@
-export const name = '$name'
+export const name = '$datumJoinDot'
 
 if (
     typeof process !== 'undefined' &&
-    process.argv[1].endsWith('/$NamePath/settings.ts')
+    process.argv[1].endsWith('/$DatumPath/settings.ts')
 ) {
     console.log(JSON.stringify({ name }))
 }
