5adba32ed4b390d0fa8f7bdc493ff4e65f92c3a0
isaac-110 <isaac-110@hi16.ca>
2023-04-16 19:56:27 +0000

    add: .gitignore

--- .scripts/@item.degit.bash
+++ .scripts/@item.degit.bash
@@ -1,9 +1,27 @@
 DIR=$(dirname $(realpath $BASH_SOURCE))
 
+declare -A type
+declare -A required
+declare -A default
+while IFS='|' read -r key type required default; do
+	KEYS+=($key)
+    type[$key]=$type
+done < <(
+    kit settings $DIR | 
+    jq -r "$(kit jq-data-select .item) | $(kit jq-bsv .key .type)"
+)
+
+
 subSchema=$(pathname sub-schema $DIR)
 [ $subSchema = '.kit-schema' ] && subSchema=schema
 data=$(string --plural -- $subSchema)
-target=$DIR/../@$data/.scripts
-mkdir -p $target
-path=$(realpath --relative-to=$(pwd) $target)
-degit hoot-camp/-data $path
+
+targetPath=$(realpath --relative-to=$(pwd) $DIR/..)
+
+for key in ${KEYS[@]}; do
+    target=$targetPath/@$key/.scripts
+    echo $target
+    mkdir -p $target
+    degit hoot-camp/-data $path
+done
+
