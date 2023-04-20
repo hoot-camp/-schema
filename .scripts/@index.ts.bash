334f885266b6a3f3bada79ced22366c23c649002
isaac-110 <isaac-110@hi16.ca>
2023-04-20 10:37:35 +0000

    update: kit script and kit.ts export option

--- .scripts/@index.ts.bash
+++ .scripts/@index.ts.bash
@@ -2,4 +2,4 @@ SRC=$(dirname $(realpath $BASH_SOURCE))
 : ${CWD:=$SRC/..}
 BASE=$(basename $BASH_SOURCE | cut -d. -f1)
 kit filter --cwd $CWD $SRC/$BASE.src.ts | 
-    kit write --prettier --cwd $CWD --base $BASE --format ts
+    kit write --kit --prettier --cwd $CWD --base $BASE --format ts
--- .scripts/createData.0.procedure.sql.bash
+++ .scripts/createData.0.procedure.sql.bash
@@ -3,4 +3,4 @@ SRC=$(dirname $(realpath $BASH_SOURCE))
 BASE=$(basename $BASH_SOURCE .sql.bash)
 mkdir -p $CWD/.mysql 
 kit filter --cwd $CWD --with-sql $SRC/$BASE.src.sql |
-    kit write --cwd $CWD/.mysql/$BASE.sql
+    kit write --cwd $CWD/.mysql --base $BASE --format sql
